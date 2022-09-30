45d43ce253a3e2389b8c92a74615d05cd0714461
tigercxx <76241201+tigercxx@users.noreply.github.com>
2022-10-01 04:37:26 +0800

    Full Skeleton built

--- digiPON/lib/main.dart
+++ digiPON/lib/main.dart
@@ -1,115 +1,155 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tryingoutbest/pages/loginpage.dart';
+import '/models/app.dart';
+import 'pages/requestercsopage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (context) => App(''), child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        // Remove debug banner
+        debugShowCheckedModeBanner: false,
+        title: 'digiPON',
+        home: const RolePage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+// Main Page
+class RolePage extends StatelessWidget {
+  const RolePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        backgroundColor: Color(0xFFFAF6ED),
+        body: Column(
+          children: [
+            Container(
+              height: 150,
+              color: Colors.transparent,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              child: Image.asset('assets/images/PSA.png'),
+            ),
+            Container(
+              height: 50,
+            ),
+            Container(
+                child: Text(
+              "Select Role",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            )),
+            Container(
+              height: 10,
+            ),
+            InkWell(
+              child: Container(
+                height: 40,
+                width: 300,
+                color: Colors.transparent,
+                child: Container(
+                  // padding: const EdgeInsets.only(left: 20, top: 2),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      border: Border.all(color: Color(0xFF505050))),
+                  child: Center(
+                    child: Text(
+                      "Requestor or Countersigning",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF505050)),
+                    ),
+                  ),
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => LoginPage(
+                            position: 1,
+                          )),
+                );
+              },
+            ),
+            Container(
+              height: 10,
+            ),
+            InkWell(
+              child: Container(
+                height: 40,
+                width: 300,
+                color: Colors.transparent,
+                child: Container(
+                  // padding: const EdgeInsets.only(left: 20, top: 2),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      border: Border.all(color: Color(0xFF129793))),
+                  child: Center(
+                    child: Text(
+                      "Designated Officer",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF129793)),
+                    ),
+                  ),
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => LoginPage(
+                            position: 2,
+                          )),
+                );
+              },
+            ),
+            Container(
+              height: 10,
+            ),
+            InkWell(
+              child: Container(
+                height: 40,
+                width: 300,
+                color: Colors.transparent,
+                child: Container(
+                  // padding: const EdgeInsets.only(left: 20, top: 2),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      border: Border.all(color: Color(0xFFFF7260))),
+                  child: Center(
+                    child: Text(
+                      "AETOS",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFFFF7260)),
+                    ),
+                  ),
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => LoginPage(
+                            position: 3,
+                          )),
+                );
+              },
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        ));
   }
 }
